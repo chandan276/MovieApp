fb8a44c29ed68dcc97264cfa01c4ed7fe5b57887
Chandan Singh <chandan276@gmail.com>
2019-03-04 18:48:40 +0530

    Initial project setup.

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Chandan Singh on 04/03/19.
+//  Copyright © 2019 Mindtree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
